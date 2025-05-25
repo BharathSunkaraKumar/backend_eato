475a4d400e88aa9646a09bdd827383cead93a5b3
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-05-25 07:29:19 +0530

    path

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+// const path = require('path');
 const verifyToken  = require('../middlewares/verifyToken');
 const firmcontroller = require('../controllers/firmcontroller');
 
@@ -9,7 +9,7 @@ router.post('/add-firm',verifyToken, firmcontroller.addFirm);
 router.get('/uploads/:imageName', (req, res) => {
     const imageName = req.params.imageName;
     res.headersSent('Content-Type', 'image/jpeg');
-    res.sendFile(path.join(__dirname, '..', '/upload', imageName));
+    res.sendFile(Path.join(__dirname, '..', '/upload', imageName));
 });
 router.delete('/:firmId', firmcontroller.deleteFirmById);
 module.exports = router;
