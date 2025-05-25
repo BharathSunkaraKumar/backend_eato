1c4032e823cceec742078f18f81cc694cc97f27d
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-05-25 09:45:41 +0530

    uploads

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -8,8 +8,8 @@ router.post('/add-firm',verifyToken, firmcontroller.addFirm);
 
 router.get('/uploads/:imageName', (req, res) => {
     const imageName = req.params.imageName;
-    res.headersSent('Content-Type', 'image/jpeg');
-    res.sendFile(path.join(__dirname, '..', 'upload', imageName));
+    res.header('Content-Type', 'image/jpeg');
+    res.sendFile(path.join(__dirname, '..', 'uploads', imageName));
 });
 router.delete('/:firmId', firmcontroller.deleteFirmById);
 module.exports = router;
